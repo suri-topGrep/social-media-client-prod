3874470bda712a4dc2bfcc5e059640d41338668f
MohanasundharamTopGrep <mohanasundharam.m@topgrep.com>
2025-11-19 13:09:50 +0530

    added empty space

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,8 @@ import TokenInput from './components/TokenInput';
 function App() {
   return (
     <div className="app-container">
-      <TokenInput />
+      <TokenInput /> 
+      
     </div>
   );
 }
